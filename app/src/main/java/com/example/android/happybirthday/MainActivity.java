5b6cfe37f9ab63af9f244ea66c384744b578be41
Ubuntu <aneemeh@gmail.com>
2016-03-27 19:03:48 +0530

    Finished App

--- app/src/main/java/com/example/android/happybirthday/MainActivity.java
+++ app/src/main/java/com/example/android/happybirthday/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.android.happybirthday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+            setContentView(R.layout.activity_main);
+
     }
 }
